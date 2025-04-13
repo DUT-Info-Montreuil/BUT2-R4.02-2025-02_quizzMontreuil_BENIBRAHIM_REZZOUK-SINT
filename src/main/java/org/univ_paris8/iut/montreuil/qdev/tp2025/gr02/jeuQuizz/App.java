8b096c5c6fdf3e6561609273050d76d4f7bc8644
Anass <benibrahimanass10@gmail.com>
2025-04-13 17:13:44 +0200

    dépendance questionnaire et joueur

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr02/jeuQuizz/App.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr02/jeuQuizz/App.java
@@ -1,10 +1,13 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr02.jeuQuizz;
 
-/**
- * Hello world!
- */
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr02.jeuQuizz.entities.dto.JoueursDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr02.jeuQuizz.entities.dto.QuestionDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr02.jeuQuizz.entities.dto.QuestionnaireDTO;
+
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
